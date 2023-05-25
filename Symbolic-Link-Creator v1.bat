1b394cd6a01ffbd2d5e0d71f4f3fd706767d6319
masumcse1 <optinfra.masum@gmail.com>
2023-05-26 00:43:18 +0600

    initialization.

---
+++ Symbolic-Link-Creator v1.bat
@@ -0,0 +1,15 @@
+@echo off
+SET modulepath=E:\myrepo
+SET destination=%modulepath%\facets\java
+SET fullOriginalPath=%modulepath%\facets\maven\src\main\java
+
+
+:SYMBOLIC_LINK_CREATION
+	MKLINK /D %fullOriginalPath% "%destination%\%foldername%"
+	ECHO Symbolic link location     : %fullOriginalPath%
+	GOTO SHUT_DOWN
+
+:SHUT_DOWN
+	ECHO --------- Done-----------------
+	PAUSE
+	EXIT
